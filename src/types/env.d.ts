def693ab9ddf3ff8dfdc1d58b7a1cdbb06d541d3
Hyunwoo Kim <admin@hyunwoo.kim>
2023-12-05 12:08:10 +0900

    Delete unused public files

--- src/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.gif';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
-declare module '*.webp';
-declare module '*.svg';
